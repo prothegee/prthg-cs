5cdc6a69a62f610d270d96d59d25f919ee596d58
prothegee <prothegee@outlook.com>
2023-07-26 16:51:34 +0700

    base utility class

---
+++ src/prthgcs/classes/Utility.cs
@@ -0,0 +1,10 @@
+namespace prthgcs
+{
+    /// <summary>
+    /// prthgcs utility class
+    /// </summary>
+    public class CUtility
+    {
+        public readonly string m_alphanumeric = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    }
+}
