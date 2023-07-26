481561b488c1742867cb1e405b3ca491daf28aad
prothegee <prothegee@outlook.com>
2023-07-26 16:51:26 +0700

    enum for status keyword

---
+++ src/prthgcs/enums/StatusEnums.cs
@@ -0,0 +1,12 @@
+namespace prthgcs
+{
+    // enum for status build
+    public enum EStatusBuild : int
+    {
+        undefined = 0,
+        development,
+        staging,
+        demo,
+        production,
+    };
+}
