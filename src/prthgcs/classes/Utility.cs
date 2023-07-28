921a20c570f99237a203cbac1c85e958d581230c
prothegee <prothegee@outlook.com>
2023-07-29 03:35:22 +0700

    v0.1.0.20230729

--- src/prthgcs/classes/Utility.cs
+++ src/prthgcs/classes/Utility.cs
@@ -14,49 +14,15 @@ namespace prthgcs
         }
 
 
-        /// <summary>
-        /// generate random number
-        /// </summary>
-        /// <note>may remove</note>
-        /// <param name="min"></param>
-        /// <param name="max"></param>
-        /// <typeparam name="T"></typeparam>
-        /// <returns></returns>
-        public T GenRandomNumber<T>(T? min, T? max)
+        public int GenerateRandomNumber(int min, int max)
         {
-            #pragma warning disable 8600
-            #pragma warning disable 8605
-
-            T result;
-
             var rd = new Random();
-
-            if (min is int && min is not null && max is int && max is not null)
-            {
-                int _min = (int)(object)min, _max = (int)(object)min;
-
-                var rs = rd.Next(_min, _max);
-                result = (T)(object)rs;
-            }
-            else if (min is double && min is not null && max is double && max is not null)
-            {
-                double _min = (double)(object)min, _max = (double)(object)min;
-
-                var rs = rd.NextDouble() * (_max - _min) + _min;
-                result = (T)(object)rs;
-            }
-            else
-            {
-                int _min = (int)(object)min, _max = (int)(object)min;
-                
-                var rs = rd.Next(_min, _max);
-                result = (T)(object)rs;
-            }
-
-            #pragma warning restore 8600
-            #pragma warning restore 8605
-
-            return result;
+            return rd.Next(min, max);
+        }
+        public double GenerateRandomNumber(double min, double max)
+        {
+            var rd = new Random();
+            return rd.NextDouble() * (max - min) + min;
         }
 
         /// <summary>
@@ -64,7 +30,7 @@ namespace prthgcs
         /// </summary>
         /// <param name="length"></param>
         /// <returns></returns>
-        public string GenRandomAlphanumeric(int length)
+        public string GenerateRandomAlphanumeric(int length)
         {
             string result = "";
 
@@ -72,7 +38,7 @@ namespace prthgcs
 
             for (int i = 0; i < length; i++)
             {
-                var j = GenRandomNumber(0, (int)m_alphanumeric.Length - 1);
+                var j = GenerateRandomNumber(0, (int)m_alphanumeric.Length - 1);
                 result += m_alphanumeric[j];
             }
 
@@ -83,10 +49,10 @@ namespace prthgcs
         /// generate random uuid
         /// </summary>
         /// <returns></returns>
-        public string GenRandomUUID()
+        public string GenerateRandomUUID()
         {
             string result;
-            string str1 = GenRandomAlphanumeric(8), str2 = GenRandomAlphanumeric(4), str3 = GenRandomAlphanumeric(4), str4 = GenRandomAlphanumeric(4), str5 = GenRandomAlphanumeric(12);
+            string str1 = GenerateRandomAlphanumeric(8), str2 = GenerateRandomAlphanumeric(4), str3 = GenerateRandomAlphanumeric(4), str4 = GenerateRandomAlphanumeric(4), str5 = GenerateRandomAlphanumeric(12);
 
             result = @$"{str1}-{str2}-{str3}-{str4}-{str5}";
 
@@ -100,7 +66,7 @@ namespace prthgcs
         /// <returns></returns>
         public bool CheckInputIsAlphabetic(string input)
         {
-            return !Regex.IsMatch(input, @"^[a-zA-Z]+$");
+            return Regex.IsMatch(input, @"^[a-zA-Z]+$");
         }
 
         /// <summary>
@@ -110,7 +76,7 @@ namespace prthgcs
         /// <returns></returns>
         public bool CheckInputIsAlphaNumeric(string input)
         {
-            return !Regex.IsMatch(input, @"^[a-zA-Z0-9]+$");
+            return Regex.IsMatch(input, @"^[a-zA-Z0-9]+$");
         }
 
         /// <summary>
@@ -148,7 +114,7 @@ namespace prthgcs
                     for (int i = 0; i < input.Length; i++)
                     {
                         // 0 is lower, 1 is upper
-                        int mixedcase = GenRandomNumber(0, 1);
+                        var mixedcase = GenerateRandomNumber(0, 2);
 
                         if (mixedcase == 0 && CheckInputIsAlphabetic(input[i].ToString()))
                         {
