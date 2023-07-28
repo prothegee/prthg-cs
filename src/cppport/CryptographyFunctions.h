a22845756f70b73cb81eeaf4484d904242abd15c
prothegee <prothegee@outlook.com>
2023-07-28 09:56:33 +0700

    cryptography class integration

--- src/cppport/CryptographyFunctions.h
+++ src/cppport/CryptographyFunctions.h
@@ -34,14 +34,144 @@ extern "C"
     /**
      * @brief generate sha1 from input
      * 
+     * @note STATUS: OK
+     * 
      * @note return length will be 40
      * 
      * @param input 
-     * @return CPPPORT_EXPORT const* 
+     * @return CPPPORT_EXPORT const char* 
      */
     CPPPORT_EXPORT const char *GenerateSHA1(const char *input);
 
+    /**
+     * @brief generate sha224 from input
+     * 
+     * @note STATUS: OK
+     * 
+     * @note return length will be 56
+     * 
+     * @param input 
+     * @return CPPPORT_EXPORT const char* 
+     */
     CPPPORT_EXPORT const char *GenerateSHA224(const char *input);
+
+    /**
+     * @brief generate sha256 from input
+     * 
+     * @note STATUS: OK
+     * 
+     * @note return length will be 64
+     * 
+     * @param input 
+     * @return CPPPORT_EXPORT const char* 
+     */
+    CPPPORT_EXPORT const char *GenerateSHA256(const char *input);
+
+    /**
+     * @brief generate sha384 from input
+     * 
+     * @note STATUS: OK
+     * 
+     * @note return length will be 96
+     * 
+     * @param input 
+     * @return CPPPORT_EXPORT const char* 
+     */
+    CPPPORT_EXPORT const char *GenerateSHA384(const char *input);
+
+    /**
+     * @brief generate sha512 from input
+     * 
+     * @note STATUS: OK
+     * 
+     * @note return length will be 128
+     * 
+     * @param input 
+     * @return CPPPORT_EXPORT const char* 
+     */
+    CPPPORT_EXPORT const char *GenerateSHA512(const char *input);
+
+    /**
+     * @brief generate blake2b from input
+     * 
+     * @note STATUS: OK
+     * 
+     * @note return length will be 128
+     * 
+     * @param input 
+     * @return CPPPORT_EXPORT const char* 
+     */
+    CPPPORT_EXPORT const char *GenerateBLAKE2b(const char *input);
+
+    /**
+     * @brief generate password hasher using scrypt
+     * 
+     * @note STATUS: OK
+     * 
+     * @param input 
+     * @return CPPPORT_EXPORT const char* 
+     */
+    CPPPORT_EXPORT const char *GenerateHasherSCRYPT(const char *input, const char *salt);
+
+    /**
+     * @brief encrypt input using XChaCha20 stream cipher
+     * 
+     * @note STATUS: OK
+     * 
+     * @note initializeKey length must 32 byte e.g. abcdefghijklmnopqrstuvwxyz123456
+     * @note initializeVector length must 24 byte e.g. abcdefghijklmnopqrstuvwx
+     * 
+     * @param input 
+     * @param initializeKey 
+     * @param initializeVector 
+     * @return CPPPORT_EXPORT const char* 
+     */
+    CPPPORT_EXPORT const char *EncryptXChaCha20(const char *input, const char *initializeKey, const char *initializeVector);
+
+    /**
+     * @brief decrypt input using XChaCha20 stream cipher
+     * 
+     * @note STATUS: OK
+     * 
+     * @note initializeKey length must 32 byte e.g. abcdefghijklmnopqrstuvwxyz123456
+     * @note initializeVector length must 24 byte e.g. abcdefghijklmnopqrstuvwx
+     * 
+     * @param input 
+     * @param initializeVector 
+     * @param initializeKey 
+     * @return CPPPORT_EXPORT const char* 
+     */
+    CPPPORT_EXPORT const char *DecryptXChaCha20(const char *input, const char *initializeKey, const char *initializeVector);
+
+    /**
+     * @brief encrypt input using CBC AES stream cipher
+     * 
+     * @note STATUS: ERROR
+     * 
+     * @note initializeKey length must 9 byte and can't start from 0 e.g. 123456789
+     * @note initializeVector length must 9 byte and can't start from 0 e.g. 987654321
+     * 
+     * @param input 
+     * @param initializeKey 
+     * @param initializeVector 
+     * @return CPPPORT_EXPORT const char* 
+     */
+    CPPPORT_EXPORT const char *EncryptCBCAES(const char *input, int initializeKey, int initializeVector);
+
+    /**
+     * @brief decrypt input using CBC AES stream cipher
+     * 
+     * @note STATUS: ERROR
+     * 
+     * @note initializeKey length must 9 byte and can't start from 0 e.g. 123456789
+     * @note initializeVector length must 9 byte and can't start from 0 e.g. 987654321
+     * 
+     * @param input 
+     * @param initializeKey 
+     * @param initializeVector 
+     * @return CPPPORT_EXPORT const char* 
+     */
+    CPPPORT_EXPORT const char *DecryptCBCAES(const char *input, int initializeKey, int initializeVector);
 }
 
 
